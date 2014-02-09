6380a3429b1da038af36c2e8cf40b674a89144f2
Mgetch <mattgetch009@yahoo.com>
2014-02-09 13:52:41 -0800

    Added the completed code as for examples
    The best build gets to be pulled into the class main file. I will be looking at brevity and efficiency over cleanliness of code. GLHF.

--- RandomWalk.cs
+++ RandomWalk.cs
@@ -9,16 +9,50 @@ namespace ARandomWalk
     {
         static void Main(string[] args)
         {
+            int stepsTaken = 0;
+            int totalSteps = 0;
+            int xPos = 0, yPos = 0;
             int i = 0;
+            float sumX = 0, sumY = 0;
+            Boolean done = true;
+
             Random random = new Random();//Create a new random instance
-            while(i<10)
+            Console.WriteLine("Pls give me the number of steps I should take!");
+            totalSteps = Convert.ToInt32(Console.ReadLine());
+
+            while (done)
             {
-                int step = random.Next(100);//generate numbers between 0 and 99
-                Console.WriteLine("{0}",step);
-                i++;
+                xPos = yPos = 0;
+                for (stepsTaken = 0; stepsTaken <= totalSteps; stepsTaken++)
+                {
+
+                    int step = random.Next(100);//generate numbers between 0 and 99
+                    switch (step % 4)
+                    {
+                        case 0:
+                            xPos++;
+                            break;
+                        case 1:
+                            xPos--;
+                            break;
+                        case 2:
+                            yPos++;
+                            break;
+                        case 3:
+                            yPos--;
+                            break;
+                    }
+                    i++;//loop counter
+                    if(i == 100)
+                        done = false;
+                    sumX += xPos;
+                    sumY += yPos;
+                } 
             }
+            sumX /= 100; // /= looks like a sad face
+            sumY /= 100;
+            Console.WriteLine("Phew! I wound up here a lot! ({0} {1})",sumX,sumY);
             Console.ReadKey();
-            //goals: I take user input for the number of steps, average the location over 100 runs. Print out the average location.
         }
     }
 }
